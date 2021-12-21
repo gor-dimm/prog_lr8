4a058ef5ba0104a3a75a83182889a726517f2e1e
Горлов Дмитрий <80451406+gor-dimm@users.noreply.github.com>
2021-12-21 14:35:47 +0300

    Added ind

---
+++ ind.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#Вариант 6
+
+def zamena(a, b):
+    def change(str):
+        str = str.replace(a, b)
+        ans = ""
+        for i in range(0, len(str)):
+            if str[i] != str[i - 1]:
+                ans += str[i]
+        return ans
+    return change
+
+
+if __name__ == '__main__':
+    print(zamena("y", "x")("yyysdyyyywqd"))
+    print(zamena("I", "You")("I love NCFU!"))
